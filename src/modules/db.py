4369154754cde27b2435dfccee5b65b660771305
dyuzha <matvey10d@gmail.com>
2025-01-09 21:34:03 +0300

    Изменил db.py

--- src/modules/db.py
+++ src/modules/db.py
@@ -1,93 +1,86 @@
 from sqlalchemy import create_engine
 from sqlalchemy import  Column, Integer, String, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, relationship
-
-db_path = "data/words.db"
-engine = create_engine("sqlite:///" + db_path, echo=True)
+from sqlalchemy.orm import DeclarativeBase, Session
 
 
-# Создаем базовый класс для моделей
 class Base(DeclarativeBase):
     pass
 
 
+class BaseUI:
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
 
-class Origin(Base):
+    def create_tables(self):
+        Base.metadata.create_all(bind=self.engine)
+        return
+
+    def drop_tables(self):
+        Base.metadata.drop_all(bind=self.engine)
+        return
+
+    def re_creation_tables(self):
+        self.drop_tables()
+        self.create_tables()
+        return
+
+
+class OriginTable(Base):
+    """Таблица основного текста"""
     __tablename__ = "origin"
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     data = Column(String, unique=True, comment="Основной текст")
 
+    def __init__(self, data):
+        self.data = data
 
 
-class Trans(Base):
+class TransTable(Base):
+    """Таблица перевода текста"""
     __tablename__ = "translates"
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                index=True)
-    id_origin = Column(Integer)
-    data = Column(String, comment="Перевод")
-    sub_text = Column(String, default=None)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    id_origin = Column(Integer, ForeignKey("origin.id"))
+    data = Column(String, comment="Переводы текста в формате json")
+    sub_text = Column(String, comment="Вспомогательный текст")
+    count = Column(Integer, default=0)
+    win = Column(Integer, default=0)
+
+    def __init__(self, data, id_origin, sub_text):
+        # Тут необходимо будет добавить проверку с помощью json_scheme
+        self.data = data
+        self.id_origin = id_origin
+        self.sub_text = sub_text
 
 
+class OriginUI:
+    """Класс для управления таблицей Origin"""
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
 
-class State(Base):
-    __tablename__ = "state"
-    id_trans = Column(Integer, ForeignKey("trans.id", ondelete="CASCADE"),
-                      unique=True)
-    id_origin = Column(Integer, ForeignKey("origin.id", ondelete="CASCADE"),
-                       primary_key=True)
-    win_trans = Column(Integer, default=0)
-    win_origin = Column(Integer, default=0)
-    loss_trans = Column(Integer, default=0)
-    loss_origin = Column(Integer, default=0)
+    def add_entry(self, data):
+        with Session(autoflush=False, bind=self.engine) as db:
+            origin = OriginTable(data)
+            db.add(origin)
+            db.commit()
+            return origin.id
 
-    id_trans = relationship( "",
-        back_populates="patient",
-        cascade="all, delete",
-        passive_deletes=True)
+    def delete_entry(self):
+        return
 
 
-    def __init__(self, id_trans, id_en, win_en=0, win_trans=0, loss_en=0, loss_trans=0):
-        self.id_tr = id_trans
-        self.id_en = id_en
-        self.win_en = win_en
-        self.win_trans = win_trans
-        self.loss_en = loss_en
-        self.loss_trans = loss_trans
+class TransUI:
+    """Класс для управления таблицей Trans"""
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
 
+    def add_entry(self, data, id_origin, sub_text):
+        with Session(autoflush=False, bind=self.engine) as db:
+            trans = TransTable(data, id_origin, sub_text)
+            db.add(trans)
+            db.commit()
+            return trans.id
 
-    # Создание виртуальных столбцов
-
-    # Победного коэффициента origin
-    @property
-    def win_coeff_origin(self):
-        return self.win_origin / self.loss_origin
-
-
-    # Победного коэффициента trans
-    @property
-    def win_coeff_trans(self):
-        return self.win_trans / self.loss_trans
-
-
-    # Общего кол-ва использований origin
-    @property
-    def all_used_origin(self):
-        return self.win_origin + self.loss_origin
-
-
-    # Общего кол-ва использований trans
-    @property
-    def all_used_trans(self):
-        return self.win_trans + self.loss_trans
-
-
-
-
-
-
-
-# Создаем таблицы
-Base.metadata.create_all(bind=engine)
-
-
+    def get_entry(self, id):
+        with Session(autoflush=False, bind=self.engine) as db:
+            trans = db.query(id=id)
+            return trans
